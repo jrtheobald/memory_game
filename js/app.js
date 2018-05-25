dde65fa242c8b55b94e70cf8142719631e972aed
James R Theobald <jrtheobald_git@icloud.com>
2018-05-24 21:53:46 -0400

    No real changes made

--- js/app.js
+++ js/app.js
@@ -71,6 +71,10 @@ card.click(function() {
   $(this).addClass('open show');
 });
 
+let openShow = document.getElementsByClassName("open show");
+console.log(openShow);
+console.log(openShow.children);
+
 /*
  * set up the event listener for a card. If a card is clicked:
  *  - display the card's symbol (put this functionality in another function that you call from this one)
